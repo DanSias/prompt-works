31270646391407c7338aa6ccbe4528afdd53ec97
DanSias <dan.sias@gmail.com>
2025-02-13 00:15:52 -0500

    Fix <Icon> error

--- src/components/categories/CategoryCards.tsx
+++ src/components/categories/CategoryCards.tsx
@@ -6,12 +6,16 @@ export default function CategoryCards() {
     <section>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6">
         {categories.map((category) => {
+          if (!category.icon) {
+            console.error(`Missing icon for category: ${category.slug}`);
+            return null; // Skip rendering if icon is missing
+          }
           const Icon = category.icon;
           return (
             <Link href={`/categories/${category.slug}`} key={category.slug}>
               <div
                 key={category.slug}
-                className="px-4 py-6 border rounded-xl shadow hover:shadow-lg transition">
+                className="bg-white px-4 py-6 border rounded-xl shadow hover:shadow-lg transition">
                 <div className="flex mb-2">
                   <Icon className="h-8 w-8 text-blue-500 mr-2" />
                   <h2 className="text-2xl font-semibold">{category.title}</h2>
