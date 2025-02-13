7cd1787ba66ef0aa005821bbe8c79ab4655a9add
DanSias <dan.sias@gmail.com>
2025-02-13 00:13:13 -0500

    Update layout and headline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,20 @@
 // app/page.tsx
 
 import CategoryCards from "@/components/categories/CategoryCards";
-import SearchBar from "@/components/SearchBar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col">
+    <main className="flex flex-col">
       <h1 className="text-4xl font-bold my-12 text-center">
-        Welcome to PromptWorks!
+        Turn Ideas Into Action with AI-Powered Prompts
       </h1>
       <p className="text-lg text-center max-w-4xl mx-auto">
-        Empower your business with AI-driven prompts for marketing, customer
-        support, data analysis, and more. Get started by exploring our
-        categories tailored to your needs.
+        Simplify marketing, customer support, data analysis, and more with our
+        easy-to-use workflows. <br />
+        Dive into categories designed to meet your business goals.
       </p>
-      <div>
-        <SearchBar />
-      </div>
-      <div className="max-w-6xl mx-auto mt-24">
+
+      <div className="max-w-6xl mx-auto mt-24 min-h-screen">
         <CategoryCards />
       </div>
     </main>
