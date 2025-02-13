9e137e9cc890d816a044a3ce44b479ade8c8f7d2
DanSias <dan.sias@gmail.com>
2025-02-13 12:17:22 -0500

    Updated headline and subhead

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,15 @@ import CategoryCards from "@/components/categories/CategoryCards";
 export default function Home() {
   return (
     <main className="flex flex-col">
-      <h1 className="text-4xl font-bold my-12 text-center">
-        Turn Ideas Into Action with AI-Powered Prompts
+      <h1 className="text-4xl font-bold mt-12 text-center">
+        From Concept to Creation with AI-Powered Support
       </h1>
-      <p className="text-lg text-center max-w-4xl mx-auto">
-        Simplify marketing, customer support, data analysis, and more with our
-        easy-to-use workflows. <br />
-        Dive into categories designed to meet your business goals.
+      <p className="text-lg text-center mt-4 max-w-4xl mx-auto">
+        Create custom AI workflows for your exact business needs. Get started in
+        minutes with our pre-built templates.
       </p>
 
-      <div className="max-w-6xl mx-auto mt-24 min-h-screen">
+      <div className="max-w-6xl mx-auto mt-16 min-h-screen">
         <CategoryCards />
       </div>
     </main>
